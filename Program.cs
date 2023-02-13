e5f3c1ff0a73ababd6559912e795addf55ca256e
YarinaRoma <romairenka@mail.ru>
2023-02-13 15:47:47 +0300

    12345

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int InNum(int number)
+{
+    int sum = 0;
+    while(number > 0)
+    {
+       number = number /10; 
+       sum += 1;
+    }
+    return sum;
+}
+Console.Write(" Input a number :");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(InNum(a));
+
